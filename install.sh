c307167f0e49320879a5a957eb3d85718bb97903
Tri Do <minhtri2582@gmail.com>
2019-12-25 08:42:47 +0700

    Install

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 echo "Installing Pfsense Wifi Marketing"
-curl https://github.com/minhtri2582/pfsense_captiveportal_wifi_marketing/archive/master.zip
+curl -L -o master.zip https://github.com/minhtri2582/pfsense_captiveportal_wifi_marketing/archive/master.zip
 unzip master.zip
-chown -Rf root:wheel *
+rm -f master.zip
+mv -rf pfsense_captiveportal_wifi_marketing-master /usr/local/captiveportal
+chown -Rf root:wheel /usr/local/captiveportal
