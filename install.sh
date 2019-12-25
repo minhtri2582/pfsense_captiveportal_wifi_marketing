3fb6da50169998a70eb97268658a34c4edcdea51
minhtri2582 <minhtri2582@gmail.com>
2019-12-25 08:44:02 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 echo "Installing Pfsense Wifi Marketing"
+rm -f master.zip
+rm -rf pfsense_captiveportal_wifi_marketing-master/
 curl -L -o master.zip https://github.com/minhtri2582/pfsense_captiveportal_wifi_marketing/archive/master.zip
 unzip master.zip
 rm -f master.zip
-mv -rf pfsense_captiveportal_wifi_marketing-master /usr/local/captiveportal
+mv -f pfsense_captiveportal_wifi_marketing-master/* /usr/local/captiveportal
+rm -rf pfsense_captiveportal_wifi_marketing-master
 chown -Rf root:wheel /usr/local/captiveportal
