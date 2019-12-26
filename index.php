c477505d336c0b68f372dc1a9889c953283982ef
minhtri2582 <minhtri2582@gmail.com>
2019-12-27 05:58:28 +0700

    Update index.php

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ function post_api($contacts)
 
 function writeLog($string)
 {
-	file_put_contents('log.txt','['.date("Y-m-d H:i:s")."]\t$string\n");
+	file_put_contents('log.txt','['.date("Y-m-d H:i:s")."]\t$string\n",FILE_APPEND);
 }
 
 function writeDb($string)
